f05d79e8abe9b61854218b101b00cedba26ec706
Jhonatan Martins <martins.cjhon@gmail.com>
2022-06-01 15:23:09 -0300

    banco de dados criado para armazenamento dos dados da sala, perguntas e acoes

--- src/route.js
+++ src/route.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index', { page: 'entrer-room' }))
 route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
-route.get('/room', (req, res) => res.render('room'))
-route.get('/room/:room/:question/:action', QuestionController.index)
+route.get('/room/:room', (req, res) => res.render('room'))
+
+// passar info do form para a url
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
+
 // para pegar um atributo nas rotas através do ejs é necessário usar a sintaxe <%=req.params.${qualparametro}%>
 
 module.exports = route
